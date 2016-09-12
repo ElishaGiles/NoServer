4081b5e6ca83b3534fd2ac48c517d6f339c0bd93
ElishaGiles <elisha.g.giles@gmail.com>
2016-09-12 16:44:35 -0600

    more updates

--- js/select-directive.js
+++ js/select-directive.js
@@ -10,17 +10,22 @@ angular.module('noServer').directive('selectDirective', function($state) {
       scope.scienceHidden = true;
       scope.distanceHidden = true;
 
+
       if($state.current.name === 'cooking'){
         scope.currencyHidden = true;
         scope.cookingHidden = false;
         scope.scienceHidden = true;
         scope.distanceHidden = true;
+        scope.typeFrom = 'teaspoon';
+        scope.typeTo = 'tablespoon';
       }
       else if($state.current.name === 'currency'){
         scope.currencyHidden = false;
         scope.cookingHidden = true;
         scope.scienceHidden = true;
         scope.distanceHidden = true;
+        scope.typeFrom = 'penny';
+        scope.typeTo = 'nickel';
 
       }
       else if($state.current.name === 'science'){
@@ -28,6 +33,8 @@ angular.module('noServer').directive('selectDirective', function($state) {
         scope.cookingHidden = true;
         scope.scienceHidden = false;
         scope.distanceHidden = true;
+        scope.typeFrom = 'ounce';
+        scope.typeTo = 'pound';
 
       }
       else if($state.current.name === 'distance'){
@@ -35,6 +42,8 @@ angular.module('noServer').directive('selectDirective', function($state) {
         scope.cookingHidden = true;
         scope.scienceHidden = true;
         scope.distanceHidden = false;
+        scope.typeFrom = 'inch';
+        scope.typeTo = 'feet';
 
       }
     }
